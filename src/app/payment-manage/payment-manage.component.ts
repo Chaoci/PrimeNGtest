0108e6f7d5a35c1040eb1e551a242c1a048151c0
Chaoci <charlieischao@gmail.com>
2023-08-09 00:07:11 +0800

    updata 8/8

---
+++ src/app/payment-manage/payment-manage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-payment-manage',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './payment-manage.component.html',
+  styleUrls: ['./payment-manage.component.css']
+})
+export class PaymentManageComponent {
+
+}
