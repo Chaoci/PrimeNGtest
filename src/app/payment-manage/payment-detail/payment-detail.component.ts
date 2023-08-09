261be8a8febc1c069211d0985db2bd2842618e39
Chaoci <charlieischao@gmail.com>
2023-08-09 23:09:35 +0800

    night work

--- src/app/payment-manage/payment-detail/payment-detail.component.ts
+++ src/app/payment-manage/payment-detail/payment-detail.component.ts
@@ -1,22 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MenuItem } from 'primeng/api';
+import { DropdownModule } from 'primeng/dropdown';
 import { TabMenuModule } from 'primeng/tabmenu';
+import { CalendarModule } from 'primeng/calendar';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-payment-detail',
   standalone: true,
   imports: [
     TabMenuModule,
+    FormsModule,
+    DropdownModule,
+    CalendarModule,
+    TableModule,
   ],
   templateUrl: './payment-detail.component.html',
   styleUrls: ['./payment-detail.component.css']
 })
+
 export class PaymentDetailComponent implements OnInit{
   items: MenuItem[]|undefined;
+  value:string|undefined;
+
+  selection: any | undefined;
+  selected: any | undefined;
+
+  date: Date | undefined = new Date();
+  products: any;
+  
   ngOnInit(): void {
-    this.items = [
-      { label: '醫令收費'},
-      { label: '醫令批價'},
+  this.items = [
+    { label: '醫令收費'},
+    { label: '醫令批價'},
   ];
-  }
+
+  this.selection = [
+    { gender: '男', code: 'TD' },
+    { gender: '女', code: 'YD' },
+    { gender: '其他', code: 'TL' },
+  ];
+  this.products = [
+    {partially:'0'},
+    
+  ]
+}
+
 }
